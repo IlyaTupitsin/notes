ceafcd495df7bd3ac4be2001069b7787c20e4805
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-10-26 14:37:10 +0300

    начало

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listview);
+
+
+        List<String> list = new ArrayList<>();
+        list.add("Заметка 1");
+        list.add("Заметка 2");
+        list.add("Заметка 3");
+        list.add("Заметка 4");
+        list.add("Заметка 5");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(MainActivity.this,textactivity.class));
+
+
+
+
+            }
+        });
+    }
+}
